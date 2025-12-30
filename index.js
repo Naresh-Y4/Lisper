85c6b97a57fa4ba821f23d56559fe96f8a3a820b
Naresh-Y4 <mr.nareshy006@gmail.com>
2025-12-30 19:10:43 +0530

    Initial commit of Lisper project

---
+++ index.js
@@ -0,0 +1,36 @@
+// index.js
+import express from "express";
+import axios from "axios";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get("https://secrets-api.appbrewery.com/random");
+    res.render("index", {
+      secret: result.data.secret,
+      user: result.data.username,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.render("index", {
+      secret: "Error fetching secret ._.",
+      user: "Unknown",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
